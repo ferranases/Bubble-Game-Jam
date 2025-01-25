e7932befdc7c1950d5cc927c56a08c90ef151876
Ferran Ases <34659733+ferranases@users.noreply.github.com>
2025-01-25 17:23:26 +0000

    a

--- Assets/PanelStart.cs
+++ Assets/PanelStart.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 public class PanelStart : MonoBehaviour
 {
     public GameObject buttonPlay;
+    public GameObject buttonSettings;
+    public GameObject settingsText;
 
+
+    private void Start()
+    {
+        settingsText.gameObject.SetActive(false);
+    }
     public void ButtonPlay()
     {
         LeanTween.scale(buttonPlay, Vector3.one * 0.9f, 0.05f).setLoopPingPong(1).setOnComplete(() =>
         {
             StartGame();
+        });
+    }
+
+    public void ButtonSettings()
+    {
+        LeanTween.scale(buttonSettings, Vector3.one * 0.9f, 0.05f).setLoopPingPong(1).setOnComplete(() =>
+        {
+            buttonSettings.gameObject.SetActive(false);
+            settingsText.transform.localScale = Vector3.zero;
+
+            settingsText.gameObject.SetActive(true);
+            LeanTween.scale(settingsText, Vector3.one, 0.05f);
         });
     }
 
